9f4f973351a00b520a67a84640bcee2062d919e2
judenns <106617684+judenns@users.noreply.github.com>
2025-12-26 18:05:43 +0000

    Update vanilla-js template

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { defineConfig } from 'vite';
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
+
+export default defineConfig({
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, 'src'),
+		},
+	},
+	build: {
+		target: 'baseline-widely-available',
+		cssMinify: false,
+		sourcemap: process.env.NODE_ENV === 'production' ? 'hidden' : true,
+	},
+});
